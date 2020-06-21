7788a3746a05821b4c29f2a007ca5b120e9599e2
shoter <lubiebardzozelki@gmail.com>
2020-06-21 12:59:14 +0200

    reworked array methods

--- src/array.ts
+++ src/array.ts
@@ -1,10 +1,5 @@
 
-export function addItemToArray<T>(array : Array<T>, newItem : T) : Array<T> {
-    let newArray : Array<T> = [];
-    array.forEach(val => newArray.push(val));
-    newArray.push(newItem);
-    return newArray;
-}
+export function addItemToArray<T>(array : Array<T>, newItem : T) : Array<T> { return [...array, newItem];}
 
 export function removeItemFromArray<T>(array : Array<T>, itemToDelete : T) : Array<T> {
     let newArray : Array<T> = [];
@@ -22,10 +17,5 @@ export function removeItemFromArray<T>(array : Array<T>, itemToDelete : T) : Arr
 }
 
 export function contains<T> (array : Array<T>, item : T) : boolean {
-    for(let val of array)
-    {
-        if(val === item)
-            return true;
-    }
-    return false;
+    return array.find((val : T, _) => val === item) === item;
 }
