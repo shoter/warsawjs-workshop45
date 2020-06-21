b0252a531fb546101aac71bf4e813e0c1f6a5076
shoter <lubiebardzozelki@gmail.com>
2020-06-21 12:52:07 +0200

    Added proper tests for bind in Typescript

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,18 +3,32 @@ import { bind } from "./index";
 interface SomeType {
     myNumber : number
 };
+describe("Bind", () => {
+    test("should be able to access this", () => {
+        let myVariable : SomeType = { myNumber: 10 };
+        function someMethod(this: SomeType) : number {
+            return this.myNumber ;
+        }
+        let bindedMethod = bind(myVariable, someMethod) as any;
+        expect(bindedMethod()).toBe(10);
+    })
 
+    test("Should be able to access parameters from binded invocation", () => {
+        let myVariable : SomeType = { myNumber: 10 };
+        function someMethod(this: SomeType, num: number) : number {
+            return this.myNumber + num;
+        }
+        let bindedMethod = bind(myVariable, someMethod) as any;
+        expect(bindedMethod(5)).toBe(15);
+    })
 
+    test("should be able to access this", () => {
+        let myVariable : SomeType = { myNumber: 10 };
+        function someMethod(this: SomeType, firstNumber: number, secondNumber : number) : number {
+            return this.myNumber + firstNumber - secondNumber;
+        }
+        let bindedMethod = bind(myVariable, someMethod, 10) as any;
+        expect(bindedMethod(5)).toBe(15);
+    })
 
-
-test("testBind", () => {
-    let myVariable : SomeType = { myNumber: 10 };
-    function someMethod(this: SomeType, otherNumber : number) : number {
-        console.log("To ja someMethod");
-        return this.myNumber;
-    }
-    let bindedMethod = bind(myVariable, someMethod) as any;
-    // expect(someMethod.bind(myVariable)(5)).toBe(15);
-    expect(bindedMethod(5)).toBe(15);
-})
-
+});
